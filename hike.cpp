42a2b86ef12a6c5796b1ad62b61518c09eaa01f9
Shreya Bhandare <shreyabhandare25@gmail.com>
2023-03-30 18:39:04 -0400

    bug fix

--- hike.cpp
+++ hike.cpp
@@ -19,7 +19,7 @@ namespace HikingProblem
     return bridges;
   }
 
-  void Hike::add_bridges(std::shared_ptr<Bridge>) {
+  void Hike::add_bridges(std::shared_ptr<Bridge> brdg) {
     bridges.push_back(brdg);
   }
 
