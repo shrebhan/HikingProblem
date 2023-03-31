2a637bf18256ff06d07489634649f69772d712a6
Shreya Bhandare <shreyabhandare25@gmail.com>
2023-03-30 21:33:56 -0400

    hardcoded input; yaml ld error

--- main.cpp
+++ main.cpp
@@ -12,9 +12,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  // std::vector<double> cross_speed {100, 50, 20, 10};
-  // unique_ptr<Hike> hike (new Hike(cross_speed));
-  // std::cout<<hike->get_fastest_bridge_crossing_time(1);
+  std::vector<double> cross_speed {100, 50, 20, 10};
 
   try
   {
@@ -23,22 +21,37 @@ int main(int argc, char *argv[])
     {
       try
       {
-        YAML::Node data = test["test"];
-        int tid = data["id"].as<int>();
-        std::vector<double> cross_speed = data["speeds"].as<std::vector<double>>();
-        int count = cross_speed.size();
+        // YAML parser yaml-cpp giving linking error
+        // tried compiling it from source, that also didn't help
+
+        // YAML::Node data = test["test"];
+        // int tid = data["id"].as<int>();
+        // std::vector<double> cross_speed = data["speeds"].as<std::vector<double>>();
+        // int count = cross_speed.size();
+        // auto hike = make_unique<Hike>(cross_speed);
+
+        // for (auto bd : data["bridge_data"])
+        // {
+        //   hike->add_bridges(std::make_shared<Bridge>(bd["length"].as<double>(), bd["add_hiker_speed"].as<std::vector<double>>()));
+        // }
+
+        //hardcoding input
+        
+        std::vector<double> cross_speed {100, 50, 20, 10};
         auto hike = make_unique<Hike>(cross_speed);
 
-        for (auto bd : data["bridge_data"])
-        {
-          hike->add_bridges(std::make_shared<Bridge>(bd["length"].as<double>(), bd["add_hiker_speed"].as<std::vector<double>>()));
-        }
+        //three bridges
+        hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{})); // only primary hikers
+        hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{2.5}));
+        hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{25, 15}));
 
-        //testing
+        //testing single bridge
         std::cout<<"bridge 0: "<<hike->get_fastest_bridge_crossing_time(0)<<std::endl;
         std::cout<<"bridge 1: "<<hike->get_fastest_bridge_crossing_time(1)<<std::endl;
         std::cout<<"bridge 2: "<<hike->get_fastest_bridge_crossing_time(2)<<std::endl;
-        // BridgeCrossing::Test(test_id, count, walk_time, crossing_time).run();
+        
+        //testing multiple bridges
+
       }
       catch (...)
       {
