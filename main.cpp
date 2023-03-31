aa8c7be9931cdf904b64c61f036c7f1dc6beb94e
Shreya Bhandare <shreyabhandare25@gmail.com>
2023-03-30 21:39:20 -0400

    final changes

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <vector>
-#include "exceptions.h"
-// #include "test.h"
-#include "yaml-cpp/yaml.h"
+//#include "yaml-cpp/yaml.h"
 #include "hike.h"
 
 using namespace HikingProblem;
@@ -12,12 +10,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  std::vector<double> cross_speed {100, 50, 20, 10};
+  std::vector<double> cross_speed{100, 50, 20, 10};
 
   try
   {
-    std::vector<YAML::Node> all_tests = YAML::LoadAllFromFile("input.yaml");
-    for (auto &test : all_tests)
+    // std::vector<YAML::Node> all_tests = YAML::LoadAllFromFile("input.yaml");
+    // for (auto &test : all_tests)
     {
       try
       {
@@ -34,24 +32,6 @@ int main(int argc, char *argv[])
         // {
         //   hike->add_bridges(std::make_shared<Bridge>(bd["length"].as<double>(), bd["add_hiker_speed"].as<std::vector<double>>()));
         // }
-
-        //hardcoding input
-        
-        std::vector<double> cross_speed {100, 50, 20, 10};
-        auto hike = make_unique<Hike>(cross_speed);
-
-        //three bridges
-        hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{})); // only primary hikers
-        hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{2.5}));
-        hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{25, 15}));
-
-        //testing single bridge
-        std::cout<<"bridge 0: "<<hike->get_fastest_bridge_crossing_time(0)<<std::endl;
-        std::cout<<"bridge 1: "<<hike->get_fastest_bridge_crossing_time(1)<<std::endl;
-        std::cout<<"bridge 2: "<<hike->get_fastest_bridge_crossing_time(2)<<std::endl;
-        
-        //testing multiple bridges
-
       }
       catch (...)
       {
@@ -59,6 +39,22 @@ int main(int argc, char *argv[])
         std::cerr << "Test inside" << std::endl;
       }
     }
+    // hardcoding input
+
+    std::vector<double> cross_speed{100, 50, 20, 10};
+    auto hike = make_unique<Hike>(cross_speed);
+
+    // three bridges
+    hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{})); // only primary hikers
+    hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{2.5}));
+    hike->add_bridges(std::make_shared<Bridge>(100, std::vector<double>{25, 15}));
+
+    // testing single bridge
+    std::cout << "bridge 0: " << hike->get_fastest_bridge_crossing_time(0) << std::endl;
+    std::cout << "bridge 1: " << hike->get_fastest_bridge_crossing_time(1) << std::endl;
+    std::cout << "bridge 2: " << hike->get_fastest_bridge_crossing_time(2) << std::endl;
+
+    // testing aggregate time of bridges
   }
   catch (...)
   {
